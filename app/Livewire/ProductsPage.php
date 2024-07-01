3c09e53955f6ec23978aa084da1113af4066b8b7
Sanjanass231 <ss2319483@gmail.com>
2024-07-01 13:42:08 +0530

    filter dynamic on products page

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -6,14 +6,48 @@ use Livewire\Component;
 use App\Models\Product;
 use App\Models\Brand;
 use App\Models\Category;
+use Livewire\Attributes\Url;
 
 #[Title('Products-Page')]
 class ProductsPage extends Component
 {
+    #[Url]
+    public $selected_categories = [];
+
+    #[Url]
+    public $selected_brands = [];
+
+    #[Url]
+    public $featured = [];
+
+    #[Url]
+    public $onSale = [];
+
+    #[Url]
+    public $priceRange = 300000;
+
     public function render()
     {
+        $productQuery = Product::query()->where('isActive',1);
+
+        if(!empty($this->selected_categories)){
+               $productQuery->whereIn('category_id',$this->selected_categories);
+        }
+        if(!empty($this->selected_brands)){
+               $productQuery->whereIn('category_id',$this->selected_brands);
+        }
+        if($this->featured){
+               $productQuery->where('isFeatured',1);
+        }
+        if($this->onSale){
+               $productQuery->where('onSale',1);
+        }
+        if($this->priceRange){
+               $productQuery->whereBetween('price',[0,$this->priceRange]);
+        }
+
         return view('livewire.products-page',[
-            'products'=> Product::query()->where('isActive',1)->paginate(6),
+            'products'=> $productQuery->paginate(6),
             'brands'=>Brand::where('isActive',1)->get(['id','name','slug']),
             'categories'=>Category::where('isActive',1)->get(['id','name','slug']),
             ''
