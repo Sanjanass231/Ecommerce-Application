1f0665d72054bb7050c793366158f7b69fb01aa7
Sanjanass231 <ss2319483@gmail.com>
2024-07-05 13:04:27 +0530

    cart page, register funtion

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
+#[Title('Register')]
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    public function save(){
+    $this->validate([
+        'name'=>'required|max:255',
+        'email'=>'required|email|unique:users|max:255',
+        'password'=>'required|min:6|max:255'
+    ]);
+    // save to database
+    $user = User::create([
+'name'=>$this->name,
+'email'=>$this->email,
+'password'=>Hash::make($this->password)
+    ]);
+
+    auth()->login($user);
+    return redirect()->intended();
+
+    }
     public function render()
-    {
+    {    
         return view('livewire.auth.register-page');
     }
 }
