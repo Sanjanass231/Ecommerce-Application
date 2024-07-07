2cadf2dd83187ea2517be3d0383affe55121c613
Sanjanass231 <ss2319483@gmail.com>
2024-07-07 15:15:52 +0530

    payment status checnge or succesfull payment as paid

--- app/Livewire/SuccessPage.php
+++ app/Livewire/SuccessPage.php
@@ -2,12 +2,36 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use App\Models\Order;
 
+#[Title('Success')]
 class SuccessPage extends Component
-{
+{    
+    #[Url]
+    public $sessionId;
+
     public function render()
     {
-        return view('livewire.success-page');
+        if($this->sessionId){
+            Stripe::setApiLey(env('STRIPE_SECRET'));
+            $sessionInfo = Session::retrieve($this->sessionId);
+
+            if($sessionInfo->paymentStatus != 'paid'){
+                $latestOrder->paymentStatus = 'failed';
+                $latestOrder->save();
+                return redirect()->route('cancel');
+            }
+            else if($sessionInfo->paymentStatus == 'paid'){
+                $latestOrder->paymentStatus = 'paid';
+                $latestOrder->save();
+                return redirect()->route('success');
+            }
+        }
+        $latestOrder = Order::with('address')->where('user_id',auth()->user()->id)->latest()->first();
+        return view('livewire.success-page',[
+            'order' => $latestOrder
+        ]);
     }
 }
