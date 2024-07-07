57fd1c5457bc90f1b7d68beeed8ec4ed013c671e
Sanjanass231 <ss2319483@gmail.com>
2024-07-07 16:06:30 +0530

    payment status checnge or succesfull payment as paid

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -3,11 +3,21 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Order;
+use Livewire\WithPagination;
+use Livewire\Attributes\Title;
 
+#[Title('My Orders')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $myOrders = Order::where('user_id',auth()->id())->latest()->paginate(2);
+
+        return view('livewire.my-orders-page',[
+            'orders'=>$myOrders
+        ]);
     }
 }
